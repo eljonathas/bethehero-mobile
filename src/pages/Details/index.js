e93fcf5c680c5e7bc481c7cbc69d1c2d4211f425
Jonathas Andrade <Jonathas.andrade5@gmail.com>
2020-03-27 01:35:56 -0300

    Update text values

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -49,7 +49,7 @@ export default function Details() {
           <Text style={styles.incidentProperty}>CASO:</Text>
           <Text style={styles.incidentValue}>{incident.description}</Text>
 
-          <Text style={styles.incidentProperty}>VALUE:</Text>
+          <Text style={styles.incidentProperty}>VALOR:</Text>
           <Text style={styles.incidentValue}>
             {Intl.NumberFormat('pt-BR', { 
               style: 'currency', 
--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -67,7 +67,7 @@ export default function Incidents() {
           <Text style={styles.incidentProperty}>CASO:</Text>
           <Text style={styles.incidentValue}>{incident.title}</Text>
 
-          <Text style={styles.incidentProperty}>VALUE:</Text>
+          <Text style={styles.incidentProperty}>VALOR:</Text>
           <Text style={styles.incidentValue}>
             {Intl.NumberFormat('pt-BR', { 
               style: 'currency', 
